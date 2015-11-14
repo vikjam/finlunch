2dcf8fcfa5f4c2f5a8078d306d1d3596634a229c
vikjam <vikjam@gmail.com>
2015-11-14 10:06:08 -0500

    Add toggle for paper

--- js/buildSchedule.js
+++ js/buildSchedule.js
@@ -12,19 +12,26 @@ xmlhttp.open("GET", url, true);
 xmlhttp.send();
 
 function myFunction(arr) {
-    var out = '<table class="table table-striped">';
-    out    += '<thead><tr><th>Date</th><th>Speaker</th><th>Paper</th></tr></thead>'
+    var out = '<table class="table">';
+    out    += '<thead><tr><th>Date</th><th>Speaker</th><th>Paper</th></tr></thead>';
     var i;
     for(i = 0; i < arr.length; i++) {
+        var paper_link  = "";
+        if (arr[i]["gsx$linktopaper"]["$t"] != "") {
+            paper_link = '<div><a href="' + arr[i]["gsx$linktopaper"]["$t"] + '#">' + '<button type="button" class="btn btn-link btn-xs"><i class="fa fa-download"></i> Full text</button></a></div>';
+        }
+        var abstract    = "";
+        var toggle_plus = "";
+        if (arr[i]["gsx$abstract"]["$t"] != "") {
+            abstract    = '<b>Abstract</b>: ' + arr[i]["gsx$abstract"]["$t"] + ' ' + paper_link;
+            toggle_plus = '<button data-toggle="collapse" data-target="#paper' + i + '" class="btn btn-link btn-xs accordion-toggle"><i class="fa fa-file-text-o"></i></button>';
+        }
         out += '<tr>' +
                '<td>' + arr[i]["gsx$date"]["$t"]    + '</td>' + 
                '<td>' + arr[i]["gsx$speaker"]["$t"] + '</td>';
-        if (arr[i]["gsx$linktopaper"]["$t"] == "") {
-            out += '<td>' + arr[i]["gsx$paper"]["$t"]   + '</td>';
-        } else {
-            out += '<td><a href="' + arr[i]["gsx$linktopaper"]["$t"] + '#">' + arr[i]["gsx$paper"]["$t"]   + '</a></td>';
-        }
+        out += '<td>' + arr[i]["gsx$paper"]["$t"]   + ' ' + toggle_plus + '</td>';
         out += '</tr>';
+        out += '<tr> <td colspan="3" class="hiddenRow"><div class="accordian-body collapse" id="paper' + i + '">' + abstract + '</div> </td> </tr>';
     }
     out += '</table>';
     document.getElementById("id01").innerHTML = out;
